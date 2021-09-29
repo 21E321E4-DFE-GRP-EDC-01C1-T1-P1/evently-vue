5cffad964c5d7deec3896108735dcde2dcbbfce0
Damaris-PC <damisoliveira@gmail.com>
2021-09-29 18:57:15 -0300

    firebase

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,28 +1,41 @@
 <template>
     <div class="login">
         <h1>Login</h1>
-        <input type="text" placeholder="Email">
+        <input type="text" placeholder="Email" v-model="email">
         <br>
-        <input type="password" placeholder="Password">
+        <input type="password" placeholder="Password" v-model="password">
         <br>
         <div class="btn-login">
-            <button>Login</button>
+            <button @click='login'>Login</button>
         </div>
         <p> You don't have an account here?
             <span><router-link to='/register'>Create an account</router-link></span></p>
     </div>
 </template>
 <script>
+import firebase from 'firebase';
 export default {
     name: "login",
     data(){
-        return{};
+        return{
+            email:'',
+            password:''
+        };
     },
     methods: {
         login: function(){
-            this.$router.replace("home");
-        }
-    }
+            firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+            .then(
+                 (user) => {
+                    this.$router.replace('home')
+                     alert(`Welcome, ${{email}}`)
+                },
+                (err) => {
+                    alert('Oops, something went wrong' + err.message)
+                }
+            );
+        },
+    },
 };
 </script>
 <style scoped></style>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,11 +1,37 @@
 <template>
     <div class="sign-up">
         <h1>Create a new account</h1>
-        <input type="text" placeholder="Email" >
+        <input type="text" placeholder="Email" v-model="email" >
         <br>
-        <input type="password" placeholder="Password">
+        <input type="password" placeholder="Password" v-model="password">
         <br>
-        <button>registry</button>
+        <button @click="signUp">registry</button>
         <span>Back to <router-link to="/login">login</router-link></span>
     </div>
 </template>
+<script>
+import firebase from "firebase";
+
+export default {
+    name: 'signUp',
+    data(){
+        return{
+            email:'',
+            password: '',
+        };
+    },
+    methods: {
+        signUp(){
+            firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+            .then(
+                (user) => {
+                    this.$router.replace('home')
+                },
+                (err) => {
+                    alert('Oops, something went wrong' + err.message)
+                }
+            );
+        },
+    },
+};
+</script>
