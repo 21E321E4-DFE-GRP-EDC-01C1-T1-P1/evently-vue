38446c9b3103d2194316e80ded1104f5b90b2ec8
Dan Spinola <daniel.trindade@al.infnet.edu.br>
2021-11-02 18:50:27 -0300

    refactor: novo setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,10 @@
 <template>
-  <div class="home">
-     <h3>Welcome</h3>
-           <p>{{user.displayName}}</p>
-           <p>{{user.email}}</p>
-        
-        <button 
-        type="submit" 
-        class="btn btn-dark btn-lg btn-block"
-        @click="logOut()">
-            Log out
-        </button>
-  </div>
+  <h2>Home</h2>
 </template>
 
 <script>
-import firebase from "firebase";
-
 export default {
-  data() {
-    return {
-      user: null
-    };
-  },
-  created() {
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        this.user = user;
-      } else {
-        this.user = null;
-      }
-    });
-  },
-  methods: {
-    logOut() {
-      firebase.auth().signOut().then(() => {
-        firebase.auth().onAuthStateChanged(() => {
-          this.$router.push('/login')
-        })
-      })
-    }
-  }
+  name: "Home",
+  components: {},
 };
 </script>
